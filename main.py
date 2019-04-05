c44686059b1e21e2c7beaf1c03b02ac3fc3c017b
CoachMac78 <amac78@gmail.com>
2019-04-04 19:17:19 -0500

    added final elements

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
     <html>
         <head>
             <style>
-                form {
+                form {{
                     background-color: #eee;
                     padding: 20px:
                     margin: 0 auto:
                     width: 540px:
                     font: 16px sans-serif;
                     border-radius: 10px;
-                }
-                textarea {
+                }}
+                textarea {{
                     margin: 10px 0;
                     width: 540px;
                     height: 120px;
-                }
+                }}
             </style>
         </head>
         <body>
@@ -32,7 +32,7 @@ form = """
                     <input type="text" name="rot" value="0" />
                     <p class="error"></p>
                 </div>
-                <textarea type="text" name="text"></textarea>
+                <textarea type="text" name="text">{0}</textarea>
                 <br>
                 <input type="submit" />
             </form>
@@ -46,11 +46,12 @@ def encrypt():
     rot_amount = int(request.form['rot'])
     info = request.form['text']
     encrypted_text = rotate_string(info, rot_amount)
-    return encrypted_text
+
+    return form.format(encrypted_text)
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 app.run()
 
